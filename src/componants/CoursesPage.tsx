ebabf320e8dda894528c46fb96ec4136f471df24
Sadeghakbary <sadeghakbary1381@gmail.com>
2025-05-26 11:39:19 +0330

    redux

---
+++ src/componants/CoursesPage.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../hooks/hooks';
+import { loadCourses } from '../redux/slices/courseSlice';
+
+const CoursesPage = () => {
+  const dispatch = useAppDispatch();
+  const { list, loading, error } = useAppSelector(state => state.course);
+
+  useEffect(() => {
+    dispatch(loadCourses());
+  }, []);
+
+  if (loading) return <p>در حال بارگذاری...</p>;
+  if (error) return <p>خطا: {error}</p>;
+
+  return (
+    <div>
+      {list.map(course => (
+        <div key={course.id}>
+          <h2>{course.title}</h2>
+          <p>{course.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CoursesPage;
